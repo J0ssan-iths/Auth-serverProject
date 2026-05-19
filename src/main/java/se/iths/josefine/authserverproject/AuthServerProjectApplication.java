1526460898174a9a05d8fbf29c6aeb23b296287b
J0ssan-iths <Josefine.Berglund@iths.se>
2026-05-19 11:27:16 +0200

    Initial commit

---
+++ src/main/java/se/iths/josefine/authserverproject/AuthServerProjectApplication.java
@@ -0,0 +1,13 @@
+package se.iths.josefine.authserverproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServerProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/iths/josefine/authserverproject/AuthServerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.josefine.authserverproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
